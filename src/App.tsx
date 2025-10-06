483405ad153c50e661b08ea17fef1b3a0f229055
24164369 <24164369@su.edu.ye>
2025-10-06 23:36:32 +0300

    Add files via upload

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import { useEffect } from 'react';
 import { motion, AnimatePresence } from 'framer-motion';
 import { Navbar, Footer } from './components/layout';
 import { Hero, About, Experience, Portfolio, Contact } from './components/sections';
